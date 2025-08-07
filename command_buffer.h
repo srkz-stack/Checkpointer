3eb445ad08c7256b52b1fbf26df9529904c1adaf
srk <sivaramakrishnan.co@gmail.com>
2025-08-07 08:23:58 +0530

    developed code for buffer command, whing act as the stagging stage for the checkpointer

---
+++ command_buffer.h
@@ -0,0 +1,21 @@
+//
+// Created by srk on 8/3/25.
+//
+
+#ifndef COMMAND_BUFFER_H
+#define COMMAND_BUFFER_H
+#include<sys/stat.h>
+#include<iostream>
+
+class bufferCommand {
+public:
+    void bufferRepo(int argc, char* argv[]);
+    void createIndexFile();
+
+private:
+
+
+};
+
+
+#endif //COMMAND_BUFFER_H
